11c79283e87968624f7273350d59e167464e6951
Stefano Pigozzi <me@steffo.eu>
2023-09-11 14:03:15 +0200

    Tweak plex configuration envvars

--- src/config.rs
+++ src/config.rs
@@ -13,9 +13,11 @@ lazy_static! {
         .parse::<u64>()
         .expect("ANGY_APPID to be a valid u64");
 
-    /// The Discord application id to use.
-    pub static ref PLEX: u64 = env::var("ANGY_PLEX")
-        .expect("ANGY_APPID to be set")
-        .parse::<u64>()
-        .expect("ANGY_APPID to be a valid u64");
+    /// The Plex server to use.
+    pub static ref PLEX_SERVER: String = env::var("ANGY_PLEX_SERVER")
+        .expect("ANGY_PLEX_SERVER to be set");
+
+    /// The Plex token to use.
+    pub static ref PLEX_TOKEN: String = env::var("ANGY_PLEX_TOKEN")
+        .expect("ANGY_PLEX_TOKEN to be set");
 }
